2dea0ad40d3d8096002503d874e5c0487488bdc6
skygrail <hilluliana@gmail.com>
2020-10-11 15:49:28 +0300

    Finished Lab_3

---
+++ Lab3/pClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_3
+{
+    partial class Abiturient
+    {
+        public void Something()
+        {
+            Console.WriteLine("Hello, world :)");
+        }
+    }
+}
