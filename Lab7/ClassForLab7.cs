abf809bedcfbca879d4c75536efe6d9ec654bb1f
skygrail <hilluliana@gmail.com>
2020-11-23 02:05:52 +0300

    Lab 7, added class of exceptions

---
+++ Lab7/ClassForLab7.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace Lab5
+{
+	class IsNotRightModel : ArgumentException
+    {
+        string Value { get; set; }
+        public IsNotRightModel (string message, string value) : base (message)
+        {
+            Value = value;
+        }
+    }
+
+    class IsNotRightPrice : Exception
+    {
+        int Value { get; set; }
+        public IsNotRightPrice(string message, int value) : base (message)
+        {
+            Value = value;
+        }
+    }
+
+    class WrongWorkingLifeValue : ArgumentOutOfRangeException
+    {
+        int Value { get; set; }
+        public WrongWorkingLifeValue(string message, int value) : base(message)
+        {
+            Value = value;
+        }
+    }
+
+    class IsNotScreenDiagonal : Exception
+    {
+        double Value { get; set; }
+        public IsNotScreenDiagonal(string message, double value) : base (message)
+        {
+            Value = value;
+        }
+    }
+}
